cbd9042a77c6e9eb2086cfb77afe8f110aa335fe
Daniel Cohen <dcohen@gatech.edu>
2013-06-26 14:18:40 -0400

    Fixed install.sh (hopefully)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-version=$(ruby -e "require 'rubygems'" -e "require 'rsquared'" -e "puts Rsquared::VERSION")
+version=$(ruby -e "require 'lib/Rsquared/version.rb'" -e "puts Rsquared::VERSION")
 
 gem build Rsquared.gemspec
 gem install Rsquared-"$version".gem
