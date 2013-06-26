78c395a2cfc8dc0ff7f40346cbcfb90bb2dcc6b9
Daniel Cohen <dcohen@gatech.edu>
2013-06-26 14:23:14 -0400

    Trying again with install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
-version=$(ruby -e "require 'lib/Rsquared/version.rb'" -e "puts Rsquared::VERSION")
+version=$(ruby -e "require './lib/Rsquared/version.rb'" -e "puts Rsquared::VERSION")
+echo $version
 
 gem build Rsquared.gemspec
-gem install Rsquared-"$version".gem
+gem install Rsquared-0.0.1.gem
 
